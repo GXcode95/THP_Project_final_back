367aab291bbbe91a0d76e56346a7ab90516e4f91
CommitID <hanxcity@gmail.com>
2021-12-07 14:43:47 +0100

    create scaffold Package

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_07_130643) do
+ActiveRecord::Schema.define(version: 2021_12_07_134259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,14 @@ ActiveRecord::Schema.define(version: 2021_12_07_130643) do
     t.date "release_date"
     t.integer "sell_stock"
     t.integer "rent_stock"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "packages", force: :cascade do |t|
+    t.integer "game_number"
+    t.string "name"
+    t.integer "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
