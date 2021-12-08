97e2f9e50ab885de31c0317932ee7dde341b7b4b
CommitID <hanxcity@gmail.com>
2021-12-08 12:03:58 +0100

    create validates user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :phone, length:{in:7..20}, uniqueness: true
+  validates :first_name, length:{in:2..20}, presence: true
+  validates :last_name, length:{in:2..20}, presence: true
+  validates :address, presence: true
   belongs_to :package
   has_many :rents
   has_many :ranks
