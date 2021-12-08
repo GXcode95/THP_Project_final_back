abc4695796bd4ad644751e049eeee2615ec1382b
ecko9 <jean.ferstler@gmail.com>
2021-12-08 17:27:45 +0100

    fix seed v2

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,6 @@ class Order < ApplicationRecord
     validates :quantity, presence: true
 
     belongs_to :cart
-    belongs_to :package
-    belongs_to :game
+    belongs_to :package, optional: true
+    belongs_to :game, optional: true
 end
