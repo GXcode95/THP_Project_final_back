441559f6a76841262bf9bc99e822c670bdf3b960
Ludovic Baratier <ludovic.baratier1@gmail.com>
2021-12-08 18:53:03 +0100

    Changing allowed params in user registrations_controller.rb

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,16 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   respond_to :json
+  before_action :configure_sign_up_params, only: [:create]
+
+  def create
+    super
+  end
 
   private
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:firstname, :lastname, :address, :phone,
+                                                       :admin, :subscription_ending, :package_id])
+  end
 
   def respond_with(resource, _opts = {})
     register_success && return if resource.persisted?
