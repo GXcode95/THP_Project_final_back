af345822d7b5dc99c0474ab85ea4d92a5c108106
Ludovic Baratier <ludovic.baratier1@gmail.com>
2021-12-09 11:38:31 +0100

    generate admin routes, adding api namespace and root

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,30 @@
 Rails.application.routes.draw do
-  resources :orders
-  resources :carts
-  resources :favorites
-  resources :comments
-  resources :ranks
-  resources :rents
-  resources :images
-  devise_for :users,
-              controllers: {
-                  sessions: 'users/sessions',
-                  registrations: 'users/registrations'
-              }
-  resources :users, only: [:show, :update]
-  resources :tags
-  resources :packages
-  resources :games
+  root to: 'games#index'
+  namespace :api do
+    resources :users, only: [:show, :update]
+    devise_for :users,
+    controllers: {
+      sessions: 'users/sessions',
+      registrations: 'users/registrations'
+    }
+    namespace :admin do
+      resources :users, only: [:index, :show]
+      resources :rents, only: [:index]
+      resources :games, only: [:index, :show, :create, :update, :destroy] do
+        resources :images, only: [:create, :destroy]
+      end
+    end
+    resources :games
+    resources :images
+    resources :carts
+    resources :orders
+    resources :packages
+    resources :rents
+  end
+  # features
+  # resources :tags
+  # resources :ranks
+  # resources :comments
+  # resources :favorites
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
