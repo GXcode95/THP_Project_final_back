09e0a1297499a9669a3f2b12b371a90f6b5c7ec6
ecko9 <jean.ferstler@gmail.com>
2021-12-09 10:38:33 +0100

    update rent enum + validations

--- app/models/rent.rb
+++ app/models/rent.rb
@@ -1,6 +1,8 @@
 class Rent < ApplicationRecord
+    enum status: [:wishlist, :rented, :past_rentals]
+
     validates :quantity, presence: true
-    validates :status, presence: true
+    validates :status
   
     belongs_to :game
     belongs_to :user
