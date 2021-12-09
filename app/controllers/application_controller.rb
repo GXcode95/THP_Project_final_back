4146d05ac8d2c6cd7e7fc54cdd9885267fd4b13e
ecko9 <jean.ferstler@gmail.com>
2021-12-09 15:54:44 +0100

    erase carts list from controller response

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::API
     @wish_list = Rent.where( user_id: user.id, status: 0 )
     @rent_games = Rent.where( user_id: user.id, status: 1 )
     @rented_games = Rent.where( user_id: user.id, status: 2 )
-    @carts = Cart.where( user_id: user.id)
     @current_cart = Cart.find_by( user_id: user.id, paid: false )
     @current_cart = Cart.create(user_id: user.id) if @current_cart == nil
     cart = {
@@ -16,7 +15,6 @@ class ApplicationController < ActionController::API
       rentedGames: @rented_games,
       rentGames: @rent_games,
       wishList: @wish_list,
-      carts: @carts,
       cart: cart
     }
   end
