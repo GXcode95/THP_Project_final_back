cb386bb84490cfa3a47f98cc78cb65d384385014
ecko9 <jean.ferstler@gmail.com>
2021-12-09 14:44:41 +0100

    setup cart controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,17 @@ class ApplicationController < ActionController::API
     @wish_list = Rent.where( user_id: user.id, status: 0 )
     @rent_games = Rent.where( user_id: user.id, status: 1 )
     @rented_games = Rent.where( user_id: user.id, status: 2 )
-    @cart = Cart.where( user_id: user.id)
-    
+    @carts = Cart.where( user_id: user.id)
+    @current_cart = Cart.find_by( user_id: user.id, paid: false )
+    @current_cart = Cart.create(user_id: user.id) if @current_cart == nil
+
     return { 
       userInfo: user, 
       rentedGames: @rented_games,
       rentGames: @rent_games,
       wishList: @wish_list,
-      cart: @cart 
+      carts: @carts,
+      current_cart: @current_cart
     }
   end
 
