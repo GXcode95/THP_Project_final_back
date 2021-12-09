ca201d9ee05833cfb422606c3427d0999972b121
GXcode95 <xavier.grenouillet@gmail.com>
2021-12-09 11:58:00 +0100

    fix status validation

--- app/models/rent.rb
+++ app/models/rent.rb
@@ -2,7 +2,6 @@ class Rent < ApplicationRecord
     enum status: [:wishlist, :rented, :past_rentals]
 
     validates :quantity, presence: true
-    validates :status
   
     belongs_to :game
     belongs_to :user
