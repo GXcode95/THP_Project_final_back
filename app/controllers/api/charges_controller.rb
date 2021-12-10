12d4f7e8a8b9784c0ea403624bf18d3eb9069a8c
Ludovic Baratier <ludovic.baratier1@gmail.com>
2021-12-10 12:06:34 +0100

    cleaning charge_controller.rb

--- app/controllers/api/charges_controller.rb
+++ app/controllers/api/charges_controller.rb
@@ -2,12 +2,8 @@ class Api::ChargesController < ApplicationController
   def create
     Stripe.api_key = ENV['SECRET_KEY']
 
-    @cart = Cart.find_by(user_id: 5, paid: false)
+    @cart = Cart.find_by(user_id: current_user, paid: false)
     @total_price = @cart.total_price
-
-    p "%" *500
-    p params[:token]
-    p "%" *500
 
     charge = Stripe::Charge.create(
       :amount => 0,
