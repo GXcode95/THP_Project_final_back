86a11c9301d44be5c4341cbf10bbdea7549943ac
Ludovic Baratier <ludovic.baratier1@gmail.com>
2021-12-10 13:27:24 +0100

    Catching errors

--- app/controllers/api/charges_controller.rb
+++ app/controllers/api/charges_controller.rb
@@ -1,18 +1,23 @@
 class Api::ChargesController < ApplicationController
   def create
     Stripe.api_key = ENV['SECRET_KEY']
-
-    @cart = Cart.find_by(user_id: current_user, paid: false)
+    
+    @cart = Cart.find_by(user_id: 6, paid: false)
     @total_price = @cart.total_price
-
+    
     charge = Stripe::Charge.create(
-      :amount => 0,
+      :amount => 100000,
       :description => 'Playbox Store',
       :currency => 'eur',
       :source => params[:id]
     )
+    rescue => e
+      error = e.message
     
-    rescue Stripe::CardError => e
-      render json: { error: e.message }
+    return generate_response(error)
+  end
+
+  def generate_response(error = nil)
+    render json: { error: error} if error
   end
 end
