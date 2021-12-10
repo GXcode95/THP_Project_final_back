92e40680f19b5083716fbe4853128ab04b786525
ecko9 <jean.ferstler@gmail.com>
2021-12-10 15:27:43 +0100

    add mailer + send welcome email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  after_create :welcome_send
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -17,4 +18,8 @@ class User < ApplicationRecord
   has_many :comments
   has_many :favorites
   has_many :carts
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
 end
