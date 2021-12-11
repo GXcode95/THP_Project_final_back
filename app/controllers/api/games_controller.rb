0b6e7363e478746b5fb177d8a3437e92000059be
ecko9 <jean.ferstler@gmail.com>
2021-12-11 20:33:23 +0100

    add images to game response

--- app/controllers/api/games_controller.rb
+++ app/controllers/api/games_controller.rb
@@ -1,9 +1,7 @@
 class Api::GamesController < ApplicationController
   # GET /games
   def index
-    @games = Game.all
-
-    render json: @games
+    render json: get_all_games()
   end
 
   # GET /games/1
@@ -11,5 +9,13 @@ class Api::GamesController < ApplicationController
     @game = Game.find(params[:id])
     
     render json: @game
+  end
+
+  private
+
+  def get_all_games
+    @all_games = Game.all
+
+    return @all_games.map { |game| { game: game, images: game.images } }
   end
 end
