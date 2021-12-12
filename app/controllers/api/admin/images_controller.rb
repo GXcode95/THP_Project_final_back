aa7f0860d988630e81671f5d4daf5f84219c7f1b
Ludovic Baratier <ludovic.baratier1@gmail.com>
2021-12-12 16:44:23 +0100

    deleting create method in image controller

--- app/controllers/api/admin/images_controller.rb
+++ app/controllers/api/admin/images_controller.rb
@@ -1,17 +1,6 @@
 class Api::Admin::ImagesController < ApplicationController
   before_action :authenticate_admin
 
-  # POST /images
-  def create
-    @image = Image.new(image_params)
-
-    if @image.save
-      render json: { messages: "L'image a bien été créée" }, status: :created, location: @image
-    else
-      render json: @image.errors, status: :unprocessable_entity
-    end
-  end
-
   # DELETE /images/1
   def destroy
     @image = Image.find(params[:id])
