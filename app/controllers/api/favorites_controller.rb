28a2212684c7f025f67eccecab51c56df5424c8b
ecko9 <jean.ferstler@gmail.com>
2021-12-13 14:08:15 +0100

    add favorites controller + routes

--- app/controllers/api/favorites_controller.rb
+++ app/controllers/api/favorites_controller.rb
@@ -1,17 +1,6 @@
 class Api::FavoritesController < ApplicationController
-  before_action :set_favorite, only: [:show, :update, :destroy]
-
-  # GET /favorites
-  def index
-    @favorites = Favorite.all
-
-    render json: @favorites
-  end
-
-  # GET /favorites/1
-  def show
-    render json: @favorite
-  end
+  before_action :set_favorite, only: [:destroy]
+  before_action :authenticate_user!
 
   # POST /favorites
   def create
@@ -19,15 +8,6 @@ class Api::FavoritesController < ApplicationController
 
     if @favorite.save
       render json: @favorite, status: :created, location: @favorite
-    else
-      render json: @favorite.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /favorites/1
-  def update
-    if @favorite.update(favorite_params)
-      render json: @favorite
     else
       render json: @favorite.errors, status: :unprocessable_entity
     end
@@ -46,6 +26,6 @@ class Api::FavoritesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def favorite_params
-      params.fetch(:favorite, {})
+      params.permit(:user_id, :game_id)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -24,9 +24,9 @@ Rails.application.routes.draw do
     resources :charges, only: [:create]
     resources :tags, only: [:index]
     resources :ranks, only: [:create]
+    resources :favorites, only: [:create, :destroy]
   end
   # features #
   # resources :comments
-  # resources :favorites
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
