32d0d042bd36fa8ea79668f5bc18028f295756de
Ludovic Baratier <ludovic.baratier1@gmail.com>
2021-12-13 19:18:33 +0100

    updating admin/games response

--- app/controllers/api/games_controller.rb
+++ app/controllers/api/games_controller.rb
@@ -10,13 +10,5 @@ class Api::GamesController < ApplicationController
     
     render json: { info: @game, images: @game.images, rank: @game.get_global_rank(), tags: @game.tags, comments: @game.comments }
   end
-
-  private
-
-  def get_all_games
-    @all_games = Game.all
-
-    return @all_games.map { |game| { info: game, images: game.images, rank: game.get_global_rank(), tags: game.tags } }
-  end
-
+  
 end
