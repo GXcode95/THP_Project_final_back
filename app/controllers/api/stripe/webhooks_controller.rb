4c980351be51e3b6ae880dc2194335a1b34463c1
Ludovic Baratier <ludovic.baratier1@gmail.com>
2021-12-17 22:26:18 +0100

    fixing error adding differnt puts

--- app/controllers/api/stripe/webhooks_controller.rb
+++ app/controllers/api/stripe/webhooks_controller.rb
@@ -6,7 +6,7 @@ class Api::Stripe::WebhooksController < ApplicationController
     payload = request.body.read
     sig_header = request.env["HTTP_STRIPE_SIGNATURE"]
     event = nil
-    p "LALALLAL"*100
+
     begin
       event = Stripe::Webhook.construct_event(
         payload, sig_header, ENV["END_POINT_SK"]
@@ -19,31 +19,21 @@ class Api::Stripe::WebhooksController < ApplicationController
       p e
       return
     end
-    p "POPPPOPOPOPOPOPOPOP"*100
+    p "#"*100
     p event.type
-    p "POPPPOPOPOPOPOPOPOP"*100
+    p "#"*100
 
     case event.type
-    when 'chekout.session.completed'
-      session = event.data.object
-      @user = User.find_by(stripe_customer_id: session.customer)
-      @user.update(subscription_status: 'active')
-      p "#"*300
-      p session
-      p "#"*300
-      p session.customer
-      p "#"*300
-      p @user.id
-      p "#"*300
-      p @user.errors
-      p "#"*300
-    when 'custome.subscription.updated', 'customer.subscription.deleted'
+    when 'customer.subscription.updated', 'customer.subscription.deleted'
       subscription = event.data.object
       @user = User.find_by(stripe_customer_id: session.customer)
       @user.update(
         subscription_status: subscription.status,
         # plan: subscription.items.data[0].price.lookup_key,
       )
+      p"$"*300
+      p @user
+      p"$"*300
     end
     render json: { message: 'success'}
   end
