a68fb75fc52d57f3f9dbc5812713049be829d25c
Ludovic Baratier <ludovic.baratier1@gmail.com>
2021-12-17 12:31:25 +0100

    Creating stripe routes and adding stripe_customer_id to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_08_172953) do
+ActiveRecord::Schema.define(version: 2021_12_17_112640) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -145,6 +145,7 @@ ActiveRecord::Schema.define(version: 2021_12_08_172953) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "stripe_customer_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["package_id"], name: "index_users_on_package_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
